a3b6fbfc6adbb99dd6bdf5b12c8da14ca9e2157b
Thibaud Colas <thibaudcolas@gmail.com>
2022-12-02 13:48:33 +0000

    Add rich text field as-is from official tutorial

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    # Copy-pasted as-is from official tutorial.
+    # See https://docs.wagtail.org/en/stable/getting_started/tutorial.html#extend-the-homepage-model.
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
